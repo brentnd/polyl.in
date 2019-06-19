9c72c6f2c61fa272fb6a5b5539793e296001348c
Brent Dimmig <brentdimmig@gmail.com>
2019-06-18 22:15:32 -0400

    initial commit with Vue app, leaflet and Bootstrap

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container mt-3 mt-sm-5" id="app">
+    <div class="row">
+      <div class="col-md-9">
+        <div class="map" id="map"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import L from 'leaflet'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'leaflet/dist/leaflet.css'
+
+export default {
+  name: "app",
+  data: function() {
+    return {
+      map: null,
+      tileLayer: null,
+      layers: []
+    };
+  },
+  mounted() {
+    this.initMap();
+  },
+  methods: {
+    initMap() {
+      this.map = L.map("map").setView([40.1304, -75.5149], 12);
+      this.tileLayer = L.tileLayer(
+        "https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png",
+        {
+          maxZoom: 18,
+          attribution:
+            '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
+        }
+      );
+
+      this.tileLayer.addTo(this.map);
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.map { height: 600px }
+</style>
